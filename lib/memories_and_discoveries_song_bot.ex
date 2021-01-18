d241afa7c1174acea9b2abdfff16ce92594581f6
ShigeruIto <sigesigeito@gmail.com>
2021-01-18 13:11:09 +0900

    init elixir project

---
+++ lib/memories_and_discoveries_song_bot.ex
@@ -0,0 +1,18 @@
+defmodule MemoriesAndDiscoveriesSongBot do
+  @moduledoc """
+  Documentation for `MemoriesAndDiscoveriesSongBot`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MemoriesAndDiscoveriesSongBot.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/memories_and_discoveries_song_bot_test.exs
@@ -0,0 +1,8 @@
+defmodule MemoriesAndDiscoveriesSongBotTest do
+  use ExUnit.Case
+  doctest MemoriesAndDiscoveriesSongBot
+
+  test "greets the world" do
+    assert MemoriesAndDiscoveriesSongBot.hello() == :world
+  end
+end
