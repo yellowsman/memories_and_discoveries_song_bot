1191d962c3b4aa8f583e848823a5d86a08cc55e9
ShigeruIto <sigesigeito@gmail.com>
2021-01-19 15:36:14 +0900

    1行で済む関数は1行に

--- lib/fetch_radiko_song_xml.ex
+++ lib/fetch_radiko_song_xml.ex
@@ -13,40 +13,30 @@ defmodule FetchRadikoSongXml do
     取得期間の開始日を返す
     TODO: 開始日をconfigから値を読み込んで設定する
   """
-  def from do
-    Enum.join([today(), "040000"])
-  end
+  def from, do: Enum.join([today(), "040000"])
 
   @doc """
     取得期間の終了日を返す
     TODO: 終了日をconfigから値を読み込んで設定する
   """
-  def to do
-    Enum.join([today(), "053000"])
-  end
+  def to, do: Enum.join([today(), "053000"])
 
   @doc """
     RadikoのAPIをリクエストしてXMLを返す
     configから読み込んだ設定で音楽リストを取得する
   """
-  def fetch do
-    fetch(station(), from(), to())
-  end
+  def fetch, do: fetch(station(), from(), to())
 
   @doc """
     RadikoのAPIをリクエストしてXMLを返す
     放送局のみconfigから読み込んだ値を使い、時刻は引数で指定する
   """
-  def fetch(from, to) do
-    fetch(station(), from, to)
-  end
+  def fetch(from, to), do: fetch(station(), from, to)
 
   @doc """
     RadikoのAPIをリクエストしてXMLを返す
   """
-  def fetch(station, from, to) do
-    HTTPoison.get!(target(station, from, to)).body
-  end
+  def fetch(station, from, to), do: HTTPoison.get!(target(station, from, to)).body
 
   # 実行時の現在日を文字列で取得する
   defp today,  do: DateTime.now!("Asia/Tokyo", Tz.TimeZoneDatabase) |> DateTime.to_iso8601(:basic) |> String.split("T") |> hd
