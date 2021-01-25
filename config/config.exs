0b8d9b16aa57242f543d1ed58416ca9609808515
ShigeruIto <sigesigeito@gmail.com>
2021-01-25 14:13:25 +0900

    configで環境変数を読み込む

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,7 @@
 import Config
 
 config :elixir, :time_zone_database, Tz.TimeZoneDatabase
+
+config :google_drive,
+  client_id: System.get_env("MAD_GOOGLE_DRIVE_API_ID"),
+  api_key: System.get_env("MAD_GOOGLE_DRIVE_API_KEY")
