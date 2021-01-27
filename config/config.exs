92dd6c95cd945ed6a40dabcf7c8f68c0331c0b82
ShigeruIto <sigesigeito@gmail.com>
2021-01-27 10:52:25 +0900

    ライブラリと設定が競合してエラーが出てるようなのでconfigから設定削除

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,3 @@
 import Config
 
 config :elixir, :time_zone_database, Tz.TimeZoneDatabase
-
-config :google_drive,
-  client_id: System.get_env("MAD_GOOGLE_DRIVE_API_ID"),
-  api_key: System.get_env("MAD_GOOGLE_DRIVE_API_KEY")
