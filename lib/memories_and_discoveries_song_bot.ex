b792aa142cab00495ca99568b1d22a890fe50b5a
ShigeruIto <sigesigeito@gmail.com>
2021-02-18 10:13:57 +0900

    DRY

--- lib/memories_and_discoveries_song_bot.ex
+++ lib/memories_and_discoveries_song_bot.ex
@@ -3,9 +3,10 @@ defmodule MemoriesAndDiscoveriesSongBot do
   Documentation for `MemoriesAndDiscoveriesSongBot`.
   """
 
-  def songlist(date, from_time, to_time), do: IO.puts(FetchRadikoSongXml.fetch(Enum.join([date, from_time]), Enum.join([date, to_time])) |> ParseRadikoSongXml.parseAtristAndTitle)
 
   def songlist(year, month, day, from_time, to_time), do: IO.puts(FetchRadikoSongXml.fetch(year, month, day, from_time, to_time) |> ParseRadikoSongXml.parseAtristAndTitle)
+
+  def songlist(date, from_time, to_time), do: songlist(Enum.join([date, from_time]), Enum.join([date, to_time]))
  
   def songlist(from, to), do: IO.puts(FetchRadikoSongXml.fetch(from, to) |> ParseRadikoSongXml.parseAtristAndTitle)
 
