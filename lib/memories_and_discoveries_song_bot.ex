913aba5d571a9840b8c60ace17b5109322516c89
ShigeruIto <sigesigeito@gmail.com>
2021-03-09 14:56:55 +0900

    関数呼び出しを明示化

--- lib/memories_and_discoveries_song_bot.ex
+++ lib/memories_and_discoveries_song_bot.ex
@@ -3,11 +3,11 @@ defmodule MemoriesAndDiscoveriesSongBot do
   Documentation for `MemoriesAndDiscoveriesSongBot`.
   """
 
-  def songlist(date), do: IO.puts(FetchRadikoSongXml.fetch(date) |> ParseRadikoSongXml.parseAtristAndTitle)
+  def songlist(date), do: FetchRadikoSongXml.fetch(date) |> ParseRadikoSongXml.parseAtristAndTitle()
 
   def songlist(date, from_time, to_time), do: songlist(Enum.join([date, from_time]), Enum.join([date, to_time]))
  
-  def songlist(from, to), do: IO.puts(FetchRadikoSongXml.fetch(from, to) |> ParseRadikoSongXml.parseAtristAndTitle)
+  def songlist(from, to), do: FetchRadikoSongXml.fetch(from, to) |> ParseRadikoSongXml.parseAtristAndTitle()
 
-  def songlist, do: IO.puts(FetchRadikoSongXml.fetch() |> ParseRadikoSongXml.parseAtristAndTitle)
+  def songlist, do: FetchRadikoSongXml.fetch() |> ParseRadikoSongXml.parseAtristAndTitle()
 end
